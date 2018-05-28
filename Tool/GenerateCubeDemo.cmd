f694aefc9cb833662d253be5e2f57f9245587701
Johan Meijdam <37294007+JohanMeijdam@users.noreply.github.com>
2018-05-28 10:24:07 +0200

    Cube Demo Model

--- Tool/GenerateCubeDemo.cmd
+++ Tool/GenerateCubeDemo.cmd
@@ -1,6 +1,6 @@
 @echo off
 set sysname=CubeDemo
-set logfile=GenerateCubeExtApiTest.log
+set logfile=GenerateCubeDemo.log
 set sysdir=Systems\%sysname%
 set db_name=composys
 set db_schema=cubetool
@@ -13,12 +13,11 @@ sqlplus.exe %db_schema%/%db_password%@%db_name% @Systems\cubetool\ModelExport.sq
 
 echo :Models
 echo Generate Models.
-::CubeGen.exe %sysdir%\CubeModel.cgm Templates\Model0.cgt %sysdir%\CubeModel0.cgm %sysname% >> %logfile% 2>&1
-
-
+CubeGen.exe %sysdir%\CubeModel.cgm Templates\DemoModel.cgt %sysdir%\CubeDemoModel.cgm %sysname% >> %logfile% 2>&1
+CubeGen.exe %sysdir%\CubeDemoModel.cgm Templates\Model0.cgt %sysdir%\CubeModel0.cgm %sysname% >> %logfile% 2>&1
 
 echo Generate Ext API Test page.
-CubeGen.exe %sysdir%\CubeModel.cgm Templates\CubeExtApiTest.cgt html\cube\cube_external_api_test.html %sysname% >> %logfile% 2>&1
+::CubeGen.exe %sysdir%\CubeModel.cgm Templates\CubeExtApiTest.cgt html\cube\cube_external_api_test.html %sysname% >> %logfile% 2>&1
 
 echo Generate Ext API Test servers.
 
