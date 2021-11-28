0794403d64c681218aa01d88a70d4191eea81ad9
Johan Meijdam <37294007+JohanMeijdam@users.noreply.github.com>
2021-11-28 21:45:57 +0100

    PG migration

---
+++ Tool/GenerateCubeToolPG.cmd
@@ -0,0 +1,17 @@
+@echo off
+set sysname=CubeTool
+set logfile=Generate%sysname%.log
+set sysdir=Systems\%sysname%
+set cubesysdir=Systems\CubeSys
+set db_name=composys
+set db_schema=cubetool
+set db_password=composys
+set wwwroot=C:\inetpub\wwwroot
+set sysroot=%wwwroot%\%sysname%
+
+echo Start > %logfile%
+echo Generate Database Views.
+CubeGen.exe %sysdir%\CubeBoModel.cgm Templates\View_pg.cgt %sysdir%\ViewDdl_pg.sql %sysname% >> %logfile% 2>&1
+set PGPASSWORD=H00rnse_H0p
+psql -h 82.217.5.32 -p 32781 -U cubetool -f %sysdir%\ViewDdl_pg.sql >> %logfile% 2>&1
+pause
