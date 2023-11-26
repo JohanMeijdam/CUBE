b696346801f846a28369c61598b3f0b77c34fb40
Johan Meijdam <37294007+JohanMeijdam@users.noreply.github.com>
2023-11-26 14:25:18 +0100

    Service page field

--- Tool/Systems/CubeTool/php/CubeToolExecuteExtractModel.php
+++ Tool/Systems/CubeTool/php/CubeToolExecuteExtractModel.php
@@ -9,7 +9,9 @@ $_SESSION['views']=0;
 -->
 </script>
 </head><body oncontextmenu="return false;" onload="InitBody()" onbeforeunload="return parent.CheckChangePending()" ondrop="Drop(event)" ondragover="AllowDrop(event)">
-<div><img src="icons/service_large.bmp" /><span> <SERVICE:U>>
+<div><img src="icons/service_large.bmp" /><span> EXTRACT_MODEL
+<table>
+<tr id="RowAtbName"><td><u><div>Name</div></u></td><td><div style="max-width:30em;"><input id="InputName" type="text" maxlength="30" style="width:100%" onchange="SetChangePending();ReplaceSpaces(this);"></input></div></td></tr>
 <tr><td><br></td><td style="width:100%"></td></tr>
 <tr><td/><td>
 <button id="ButtonExec" type="button" disabled>Execute</button>&nbsp;&nbsp;&nbsp;
