189d23faac16cc1dbe6e7b6010294d1938afead6
Johan Meijdam <37294007+JohanMeijdam@users.noreply.github.com>
2024-01-02 21:44:35 +0100

    System User

--- Tool/GenerateCubeTool.cmd
+++ Tool/GenerateCubeTool.cmd
@@ -17,8 +17,8 @@ echo Start > %logfile%
 ::goto ModelImport
 ::goto ModelExport
 ::goto Packages
-goto Application 
-::goto System
+::goto Application 
+goto System
 echo Extract Cube Model
 sqlplus.exe cuberoot/composys@composys @Systems\CubeRoot\ModelExport.sql %sysdir%\CubeModel.cgm %sysname% REPLACE >> %logfile% 2>&1
 ::goto End
